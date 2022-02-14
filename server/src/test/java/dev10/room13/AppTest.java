6e0f296800510db3e4b0c4910871460f35a70b91
jkesle <joshua.e.kesler@gmail.com>
2022-02-14 10:08:03 -0600

    add server skeleton

---
+++ server/src/test/java/dev10/room13/AppTest.java
@@ -0,0 +1,20 @@
+package dev10.room13;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
