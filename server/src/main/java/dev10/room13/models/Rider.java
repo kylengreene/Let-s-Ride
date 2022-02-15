641af56d27d18f5cbff977b34e0345c67b25923f
jkesle <joshua.e.kesler@gmail.com>
2022-02-15 14:55:54 -0600

    Rider.java

--- server/src/main/java/dev10/room13/models/Rider.java
+++ server/src/main/java/dev10/room13/models/Rider.java
@@ -1,5 +1,49 @@
 package dev10.room13.models;
 
 public class Rider {
-    
+
+    private int riderId;
+    private String riderFirstname;
+    private String riderLastname;
+    private String riderPostal;
+    private String riderUsername;
+    private String riderPassword;
+
+
+    public int getRiderId() {
+        return riderId;
+    }
+    public void setRiderId(int riderId) {
+        this.riderId = riderId;
+    }
+    public String getRiderFirstname() {
+        return riderFirstname;
+    }
+    public void setRiderFirstname(String riderFirstname) {
+        this.riderFirstname = riderFirstname;
+    }
+    public String getRiderLastname() {
+        return riderLastname;
+    }
+    public void setRiderLastname(String riderLastname) {
+        this.riderLastname = riderLastname;
+    }
+    public String getRiderPostal() {
+        return riderPostal;
+    }
+    public void setRiderPostal(String riderPostal) {
+        this.riderPostal = riderPostal;
+    }
+    public String getRiderUsername() {
+        return riderUsername;
+    }
+    public void setRiderUsername(String riderUsername) {
+        this.riderUsername = riderUsername;
+    }
+    public String getRiderPassword() {
+        return riderPassword;
+    }
+    public void setRiderPassword(String riderPassword) {
+        this.riderPassword = riderPassword;
+    }
 }
