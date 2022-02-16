9daa1d10a3f6dc6dec2b4ac9dae8e5305561b7bb
kylengreene <kylengreene@gmail.com>
2022-02-16 11:20:00 -0600

    commit after blocking out components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar.js";
+import HomePage from "./components/HomePage";
+import AccountProfile from "./components/AccountProfile";
+import SignUp from "./components/SignUp";
+import ClubAdminPage from "./components/ClubAdminPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/account" exact>
+            <AccountProfile />
+          </Route>
+          <Route path="/signup" exact>
+            <SignUp />
+          </Route>
+          <Route path="/admin" exact>
+            <ClubAdminPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
