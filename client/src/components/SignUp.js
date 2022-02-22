6d41a361a053f5830737546cc195cb474839e091
kylengreene <kylengreene@gmail.com>
2022-02-22 13:45:22 -0600

    fixed import bug

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -13,7 +13,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {createRider} from "../api/signup.js";
+import {createRider} from "../api/rider-api";
 
 function Copyright(props) {
   return (
