e8e5eced2aebd35525eff2e678a71bf8bfa3f427
Isaiah Rodriguez <ifrod18@gmail.com>
2022-02-24 14:02:31 -0600

    added edit to clubform

--- client/src/api/club.js
+++ client/src/api/club.js
@@ -15,3 +15,28 @@ export async function findClubs() {
         return Promise.reject(response.status);
     }
 }
+
+export async function findById(clubId) {
+
+    const init = { method: "GET", headers: { "Authorization": `Bearer ${localStorage.getItem("TOKEN")}` } };
+
+    const response = await fetch(`${baseUrl}/${clubId}`, init);
+    if (response.status === 200) {
+        return response.json();
+    } else if (response.status === 403) {
+        return Promise.reject(403);
+    }
+    return Promise.reject("Could not fetch club.");
+}
+
+export async function saveClubData(club) {
+    return club.id > 0 ? update(club) : add(club);
+}
+
+async function update(club) {
+    console.log("club updated");
+}
+
+async function add(club) {
+    console.log("club added");
+}
