c31808517bae6bd92f6fdaf392d602ccd085e01f
kylengreene <kylengreene@gmail.com>
2022-03-02 09:32:05 -0600

    commit before bailing

--- client/src/sagas/rootSagas.js
+++ client/src/sagas/rootSagas.js
@@ -2,5 +2,5 @@ import { all } from "redux-saga/effects";
 import watcherClubSaga from "./handlers/fetchClubs";
 
 export default function* rootSaga(){
-    yield all([watcherClubSaga(), ])
+    yield all([watcherClubSaga() ])
 }
